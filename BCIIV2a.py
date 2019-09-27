1c47f0a2b9540e541c0f6ddd4620f0673d7474db
Boxann John <597812710@qq.com>
2019-09-27 18:26:36 +0800

    save

--- BCIIV2a.py
+++ BCIIV2a.py
@@ -19,5 +19,5 @@ K.set_image_data_format('channels_last')
 
 if __name__=='__main__':
     #train_EEGNet(4, srate=srate, prep=False)
-    train_rawEEGConvNet(4, srate=srate, Colors=5)
+    train_rawEEGConvNet(4, srate=srate)
     pass
